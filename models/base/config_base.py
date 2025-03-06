50d1fb84e8eec89768408c83b341cf5f2f547dfc
shadowforce78 <planque.adam@gmail.com>
2025-03-06 09:12:55 +0100

    Remove deprecated configuration and scripts; update .gitignore to streamline file management

--- models/base/config_base.py
+++
@@ -1,7 +0,0 @@
-# Configuration des chemins et paramètres communs
-MODEL_BASE = "dbddv01/gpt2-french-small"
-SPECIAL_TOKENS = {
-    "pad_token": "<|pad|>",
-    "bos_token": "<|bos|>",
-    "eos_token": "<|eos|>",
-}
